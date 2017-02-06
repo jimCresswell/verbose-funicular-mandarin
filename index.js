817c78a387c11313cc0dd964b49d9db9d282408c
jimCresswell <jim.cresswell@gmail.com>
2017-02-06 12:04:01 +0000

    Implement scoring.

--- index.js
+++ index.js
@@ -20,17 +20,42 @@
 // to be optional.
 const calledFromCommandLine = (require.main === module);
 
-// Configuration data.
+// Application Configuration data.
 const config = {
   defaultFilePath: 'heptathlon.csv',
   newlineRegex: /\n|\r\n|\r/,
-  csvSeparator: ',',
-  fieldIndex: {
-    athlete: 0,
-    competition: 1,
-    result: 2,
-    datetime: 3
-  }
+  csvSeparator: ','
+};
+
+// const fieldIndex = {
+//   athlete: 0,
+//   competition: 1,
+//   result: 2,
+//   datetime: 3
+// };
+
+/**
+ * Event type and point weightings for each event.
+ * @type {Object}
+ */
+const eventConfig = {
+  '200m': { type: 'running', weights: { A: 4.99087, B: 42.5, C: 1.81 } },
+  '800m': { type: 'running', weights: { A: 0.11193, B: 254, C: 1.88 } },
+  '100m': { type: 'running', weights: { A: 9.23076, B: 26.7, C: 1.835 } },
+  'high': { type: 'jumping', weights: { A: 1.84523, B: 75.0, C: 1.348 } },
+  'long': { type: 'jumping', weights: { A: 0.188807, B: 210, C: 1.41 } },
+  'shot': { type: 'throwing', weights: { A: 56.0211, B: 1.50, C: 1.05 } },
+  'javelin': { type: 'throwing', weights: { A: 15.9803, B: 3.80, C: 1.04 } }
+};
+
+/**
+ * For a given event type and result calculate the score.
+ * @type {Object}
+ */
+const calcScores = {
+  running: (result, A, B, C) => Math.floor(A * Math.pow((B - result), C)),
+  throwing: (result, A, B, C) => Math.floor(A * Math.pow((result - B), C)),
+  jumping: (result, A, B, C) => Math.floor(A * Math.pow(((result * 100) - B), C))
 };
 
 
@@ -117,5 +142,7 @@ if (calledFromCommandLine) {
  */
 
 module.exports = {
-  getSummary: getSummary
+  getSummary: getSummary,
+  eventConfig: eventConfig,
+  calcScores: calcScores
 };
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 require('chai').should();
 
-var calcScores = require('../index.js');
+var heptathlon = require('../index.js');
 
 const inputData = require('./data/inputData');
 const outputData = require('./data/outputData');
@@ -8,6 +8,52 @@ const outputData = require('./data/outputData');
 // Integration test.
 describe('The whole system', function () {
   it('Should map the test input to test output.', function () {
-    calcScores.getSummary(inputData).should.equal(outputData);
+    heptathlon.getSummary(inputData).should.equal(outputData);
+  });
+});
+
+// Points calculation
+describe('Point scoring', function () {
+  it('Should match the provided 100m example', function () {
+    const event = '100m';
+    const result = 16.2; // Seconds
+
+    const eventConfig = heptathlon.eventConfig[event];
+    const eventType = eventConfig.type;
+    const weights = eventConfig.weights;
+    const calcScore = heptathlon.calcScores[eventType];
+
+    const points = calcScore(result, weights.A, weights.B, weights.C);
+
+    points.should.equal(690);
+  });
+
+  // https://en.wikipedia.org/wiki/Heptathlon#Women.27s_world_records_compared_to_heptathlon_bests
+  it('Should match the long jump example on Wikipedia', function () {
+    const event = 'long';
+    const result = 7.27; // Metres
+
+    const eventConfig = heptathlon.eventConfig[event];
+    const eventType = eventConfig.type;
+    const weights = eventConfig.weights;
+    const calcScore = heptathlon.calcScores[eventType];
+
+    const points = calcScore(result, weights.A, weights.B, weights.C);
+
+    points.should.equal(1264);
+  });
+
+  it('Should match the javelin example on Wikipedia', function () {
+    const event = 'javelin';
+    const result = 60.90; // Metres
+
+    const eventConfig = heptathlon.eventConfig[event];
+    const eventType = eventConfig.type;
+    const weights = eventConfig.weights;
+    const calcScore = heptathlon.calcScores[eventType];
+
+    const points = calcScore(result, weights.A, weights.B, weights.C);
+
+    points.should.equal(1072);
   });
 });
